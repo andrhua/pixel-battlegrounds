4e11793da2a2bb419c9fa88cc81f4f73fedb8596
andrhua <ndrygy@ya.ru>
2019-04-28 23:55:14 +0300

    added anonymous authorization

--- core/login_screen.py
+++ core/login_screen.py
@@ -18,11 +18,18 @@ from util.decorators import threaded
 class LoginScreen(Screen):
     def __init__(self, context):
         super().__init__(context)
-        self.active_form = None
-        self.forms = [self.get_widget('email_form'), self.get_widget('password_form')]
-        email, password = self.load(util.constants.LOCAL_SAVE_EMAIL), self.load(util.constants.LOCAL_SAVE_PASSWORD)
-        if email is not None:
-            self.login(email, password)
+        self.login_anonymously()
+        # self.active_form = None
+        # self.forms = [self.get_widget('email_form'), self.get_widget('password_form')]
+        # email, password = self.load(util.constants.LOCAL_SAVE_EMAIL), self.load(util.constants.LOCAL_SAVE_PASSWORD)
+        # if email is not None:
+        #     self.login(email, password)
+
+    @threaded
+    def login_anonymously(self):
+        self.get_widget('loader').enabled = True
+        self.context.user = self.context.auth.sign_in_anonymous()
+        self.context.game.set_game_screen()
 
     @threaded
     def login(self, email, password):
@@ -78,23 +85,23 @@ class LoginScreen(Screen):
         style_status = TextLabelStyle(Assets.font_small, Colors.GREY, None, Align.center)
         style_edit_text = TextFormStyle(Assets.font_regular, Colors.BLACK, Colors.GREY, Colors.WHITE, Align.center)
         self.add_widget('app_label', TextLabel(i18n.APP_NAME,
-                                               Constants.SCREEN_WIDTH / 2, .25 * Constants.SCREEN_HEIGHT,
+                                               Constants.SCREEN_WIDTH / 2, .5 * Constants.SCREEN_HEIGHT,
                                                style_logo))
-        self.add_widget('login', TextLabel('sign in / register',
-                                           Constants.SCREEN_WIDTH / 2, .45 * Constants.SCREEN_HEIGHT,
-                                           style_regular))
-        self.add_widget('email_form', TextForm(i18n.EMAIL_HINT,
-                                               Constants.SCREEN_WIDTH / 2, .55 * Constants.SCREEN_HEIGHT,
-                                               style_edit_text))
-        self.add_widget('password_form', TextForm(i18n.PASSWORD_HINT,
-                                                  Constants.SCREEN_WIDTH / 2, .65 * Constants.SCREEN_HEIGHT,
-                                                  style_edit_text, True))
+        # self.add_widget('login', TextLabel('sign in / register',
+        #                                    Constants.SCREEN_WIDTH / 2, .45 * Constants.SCREEN_HEIGHT,
+        #                                    style_regular))
+        # self.add_widget('email_form', TextForm(i18n.EMAIL_HINT,
+        #                                        Constants.SCREEN_WIDTH / 2, .55 * Constants.SCREEN_HEIGHT,
+        #                                        style_edit_text))
+        # self.add_widget('password_form', TextForm(i18n.PASSWORD_HINT,
+        #                                           Constants.SCREEN_WIDTH / 2, .65 * Constants.SCREEN_HEIGHT,
+        #                                           style_edit_text, True))
         self.add_widget('loader', SpriteImage(
             Constants.SCREEN_WIDTH / 2 - Constants.FRAME_WIDTH / 2, .78 * Constants.SCREEN_HEIGHT))
-        self.add_widget('login_feedback', TextLabel('',
-                                                    Constants.SCREEN_WIDTH / 2, 9 * Constants.SCREEN_HEIGHT / 10,
-                                                    style_status))
-        self.get_widget('login_feedback').enabled = False
+        # self.add_widget('login_feedback', TextLabel('',
+        #                                             Constants.SCREEN_WIDTH / 2, 9 * Constants.SCREEN_HEIGHT / 10,
+        #                                             style_status))
+        # self.get_widget('login_feedback').enabled = False
 
     def get_active_form(self):
         return self.get_widget(self.active_form)
@@ -109,33 +116,35 @@ class LoginScreen(Screen):
             self.get_active_form().hit(self.get_active_form().x, self.get_active_form().y)
 
     def on_key_down(self, key, unicode):
-        if self.active_form is not None:
-            form = self.get_active_form()
-            if key == pygame.K_LEFT:
-                form.move_cursor(-1)
-            elif key == pygame.K_RIGHT:
-                form.move_cursor(1)
-            elif key == pygame.K_BACKSPACE:
-                form.delete_symbol()
-            elif key == pygame.K_TAB:
-                self.switch_active_form()
-            elif key == pygame.K_RETURN:
-                if self.get_widget('password_form').get_text() == '':
-                    if self.active_form == 'email_form':
-                        self.switch_active_form()
-                else:
-                    self.get_widget('login_feedback').enabled = True
-                    email, password = self.get_widget('email_form'), self.get_widget('password_form')
-                    email.is_editable = False
-                    email.is_editable = False
-                    self.login(email.get_text(), password.get_text())
-            else:
-                form.append_text(unicode)
+        pass
+        # if self.active_form is not None:
+        #     form = self.get_active_form()
+        #     if key == pygame.K_LEFT:
+        #         form.move_cursor(-1)
+        #     elif key == pygame.K_RIGHT:
+        #         form.move_cursor(1)
+        #     elif key == pygame.K_BACKSPACE:
+        #         form.delete_symbol()
+        #     elif key == pygame.K_TAB:
+        #         self.switch_active_form()
+        #     elif key == pygame.K_RETURN:
+        #         if self.get_widget('password_form').get_text() == '':
+        #             if self.active_form == 'email_form':
+        #                 self.switch_active_form()
+        #         else:
+        #             self.get_widget('login_feedback').enabled = True
+        #             email, password = self.get_widget('email_form'), self.get_widget('password_form')
+        #             email.is_editable = False
+        #             email.is_editable = False
+        #             self.login(email.get_text(), password.get_text())
+        #     else:
+        #         form.append_text(unicode)
 
     def on_mouse_click(self):
-        x, y = pygame.mouse.get_pos()
-        for name, w in self.widgets.items():
-            is_hit, widget = w.hit(x, y)
-            if is_hit:
-                self.active_form = name if widget == 'edit' else None
-                return
+        pass
+        # x, y = pygame.mouse.get_pos()
+        # for name, w in self.widgets.items():
+        #     is_hit, widget = w.hit(x, y)
+        #     if is_hit:
+        #         self.active_form = name if widget == 'edit' else None
+        #         return
